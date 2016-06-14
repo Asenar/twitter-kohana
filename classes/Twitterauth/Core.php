9b90b4c5ab70e6379c30aa802401cf89b94698c2
Koen Pasman <koen@aubergine-it.nl>
2016-06-14 21:33:45 +0200

    More fixes in the code, not really waterproof yet

--- classes/Twitterauth/Core.php
+++ classes/Twitterauth/Core.php
@@ -3,7 +3,7 @@
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 class Twitterauth_Core extends TwitterOAuth {
-	public $request_token = array();
+	public $request_token = [];
 	public static $token_name = 'twitterauth_tokens';
 	public $config;
 
@@ -23,9 +23,7 @@ class Twitterauth_Core extends TwitterOAuth {
 	{
 		if (!isset($_SESSION[self::$token_name]))
 		{
-			$this->request_token = $this->oauth('oauth/request_token', ['oauth_callback' => URL::site('account/twittercb', Request::$current)]);
-			$_SESSION[self::$token_name] = $this->request_token;
-			$this->setOauthToken($this->request_token['oauth_token'], $this->request_token['oauth_token']);
+			$this->getRequestToken();
 		}
 		else
 		{
@@ -36,6 +34,14 @@ class Twitterauth_Core extends TwitterOAuth {
 		return $this;
 	}
 
+	function getRequestToken()
+	{
+		$this->request_token = $this->oauth('oauth/request_token', ['oauth_callback' => URL::site('account/twittercb', Request::$current)]);
+		$_SESSION[self::$token_name] = $this->request_token;
+		$this->setOauthToken($this->request_token['oauth_token'], $this->request_token['oauth_token_secret']);
+		return $this->request_token;
+	}
+
 	/**
 	 * @param null $token
 	 * @return string
@@ -44,7 +50,7 @@ class Twitterauth_Core extends TwitterOAuth {
 	function getAuthorizeURL($token = null)
 	{
 		$_SESSION['twitterauth_url'] = URL::site(Request::$current->detect_uri(), Request::$current);
-		$oauth_token = $token ? : $this->request_token['oauth_token'];
+		$oauth_token = $token ? : isset($this->request_token['oauth_token']) ? $this->request_token['oauth_token'] : $this->getRequestToken();
 		return $this->url('oauth/authorize', ['oauth_token' => $oauth_token]);
 	}
 
@@ -57,12 +63,15 @@ class Twitterauth_Core extends TwitterOAuth {
 	 * - x_auth_expires
 	 * @param null $oauth_verifier
 	 * @throws \Abraham\TwitterOAuth\TwitterOAuthException
+	 * @return array
 	 */
 	function getAccessToken($oauth_verifier = null)
 	{
 		$access_token = $this->oauth('oauth/access_token', ['oauth_verifier' => $oauth_verifier]);
 		$_SESSION[self::$token_name] = $access_token;
 		$this->setOauthToken($access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+		return $access_token;
 	}
 
 	/**
@@ -75,6 +84,13 @@ class Twitterauth_Core extends TwitterOAuth {
 		{
 			return $user;
 		}
+
+		if (isset($user->errors[0]->code) && $user->errors[0]->code == 89)
+		{
+			// We've encountered an invalid or expired token
+			unset($_SESSION[self::$token_name]);
+		}
+
 		return false;
 	}
 
